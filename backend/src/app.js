420393ada02e99e402185fa93cf5ed06f5bc14fc
Nathan <nbel806@aucklanduni.ac.nz>
2024-04-03 13:16:32 +1300

    build: backend basic setup

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(5000, function () {
+   console.log("Express App running at http://127.0.0.1:5000/");
+})
