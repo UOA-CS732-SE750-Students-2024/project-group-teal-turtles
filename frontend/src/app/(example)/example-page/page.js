7b371f135b8cf28b84ef9ba6219223c98a3aa823
Jason <jasxavier7@gmail.com>
2024-04-08 18:13:25 +1200

    feat: made an example page and component

---
+++ frontend/src/app/(example)/example-page/page.js
@@ -0,0 +1,12 @@
+import ExampleComponent from "@/components/ExampleComponent/ExampleComponent";
+import React from "react";
+
+function ExamplePage() {
+	return (
+		<>
+			<ExampleComponent />
+		</>
+	);
+}
+
+export default ExamplePage;
