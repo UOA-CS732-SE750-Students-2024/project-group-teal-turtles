2d4aeca24388b7b2e9183e240960e26c49214aee
tmur647 <tmur647@aucklanduni.ac.nz>
2024-04-17 11:26:28 +1200

    fix: fixed prettierc and applied it to all files

--- backend/src/data/user-dao.js
+++ backend/src/data/user-dao.js
@@ -1,44 +1,44 @@
 import { User } from "./schema.js";
 export async function retrieveUser(uid) {
-  return await User.findById(uid);
+	return await User.findById(uid);
 }
 
 export async function createUser(uid) {
-  const newUser = new User({
-    _id: uid
-  });
-  await newUser.save();
-  return newUser;
+	const newUser = new User({
+		_id: uid
+	});
+	await newUser.save();
+	return newUser;
 }
 
 export async function addFavouriteMeal(uid, newfavouriteMeal) {
-  return await User.findByIdAndUpdate(uid, { $addToSet: { favouriteMeals: newfavouriteMeal } });
+	return await User.findByIdAndUpdate(uid, { $addToSet: { favouriteMeals: newfavouriteMeal } });
 }
 
 export async function deleteFavouriteMeal(uid, favouriteMealToDelete) {
-  return await User.findById(uid, { $pull: { favouriteMeals: favouriteMealToDelete } });
+	return await User.findById(uid, { $pull: { favouriteMeals: favouriteMealToDelete } });
 }
 
 export async function addGeneratedMeal(uid, generatedMeal) {
-  return await User.findByIdAndUpdate(uid, { $addToSet: { generatedMeals: generatedMeal } });
+	return await User.findByIdAndUpdate(uid, { $addToSet: { generatedMeals: generatedMeal } });
 }
 
 export async function setPantry(uid, pantry) {
-  return await User.findByIdAndUpdate(uid, { pantry: pantry });
+	return await User.findByIdAndUpdate(uid, { pantry: pantry });
 }
 
 export async function setDislikedIngredientsList(uid, dislikedIngredients) {
-  return await User.findByIdAndUpdate(uid, { dislikedIngredients: dislikedIngredients });
+	return await User.findByIdAndUpdate(uid, { dislikedIngredients: dislikedIngredients });
 }
 
 export async function addDislikedIngredient(uid, dislikedIngredient) {
-  return await User.findByIdAndUpdate(uid, { $addToSet: { dislikedIngredients: dislikedIngredient } });
+	return await User.findByIdAndUpdate(uid, { $addToSet: { dislikedIngredients: dislikedIngredient } });
 }
 
 export async function setUserParameters(uid, parameters) {
-  return await User.findByIdAndUpdate(uid, { parameters: parameters });
+	return await User.findByIdAndUpdate(uid, { parameters: parameters });
 }
 
 export async function deleteUser(uid) {
-  await User.findByIdAndDelete(uid);
+	await User.findByIdAndDelete(uid);
 }
--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", function (req, res) {
-  res.send("Hello World");
+	res.send("Hello World");
 });
 
 export default router;
