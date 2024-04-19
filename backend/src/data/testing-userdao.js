f5d8994fc91b12b0c305649c3de74049741d277d
tmur647 <100457787+tmur647@users.noreply.github.com>
2024-04-19 18:38:11 +1200

    feat: create schemas (#11)
    * style: tabs and more width
    
    * build: add to git ignore
    
    * build: backend basic setup
    
    * feat: add middleware to backend
    
    * feat: add basic routing to backend
    
    * feat: implement initial schema
    
    * fix: apply prettier to all files
    
    * fix: fixed prettierc and applied it to all files
    
    ---------
    
    Co-authored-by: Nathan <nbel806@aucklanduni.ac.nz>

---
+++ backend/src/data/testing-userdao.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+
+await mongoose.connect(process.env.DB_URL);
+console.log("Connected to database");
+
+import { User } from "./schema.js";
+import { retrieveUser, createUser, addFavouriteMeal } from "./user-dao.js";
+
+User.deleteMany({})
+	.then(() => createUser("id1"))
+	.then(() => addFavouriteMeal("id1", "cheese burger"))
+	.then(() => {
+		return retrieveUser("id1");
+	})
+	.then((user) => {
+		console.log(user.favouriteMeals);
+	});
