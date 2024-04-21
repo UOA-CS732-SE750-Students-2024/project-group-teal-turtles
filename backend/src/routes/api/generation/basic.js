33c50bb8441bceb6bcc360d075f92705978b13cd
Nathan <nbel806@aucklanduni.ac.nz>
2024-04-21 21:10:45 +1200

    get string object

--- backend/src/routes/api/generation/basic.js
+++ backend/src/routes/api/generation/basic.js
@@ -5,11 +5,11 @@ import createAndRun from "../../../openAI/openAi.js";
 const basic = express.Router();
 
 basic.get("/", async (req, res) => {
-	var body = req.body;
+	var body = req.body.string;
 	const levelOfStrictness = "Level of strictness for ingredients: Strict";
 	body = body + levelOfStrictness;
-	const text = await createAndRun(process.env.ASSISTANT_ID, body);
-	res.send(JSON.stringify(text));
+	console.log(body);
+	res.send(JSON.stringify(await createAndRun(process.env.ASSISTANT_ID, body)));
 });
 
 export default basic;
