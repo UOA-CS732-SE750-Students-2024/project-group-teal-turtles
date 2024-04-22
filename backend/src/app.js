636f08bc6f581fe8edcc3d1f805c110892921889
Nathan <nbel806@aucklanduni.ac.nz>
2024-04-22 16:18:41 +1200

    refactor: cleanup

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,3 @@ app.use("/", routes);
 await mongoose.connect(process.env.DB_URL);
 const PORT = process.env.PORT ?? 3000;
 app.listen(3000, () => console.log(`Express Server is up and running on ${PORT}`));
-
-// const body =
-// 	" { Ingredients you can include: Potato, Mushrooms, Cheese, Milk, Flour, Eggs, Rasins, Apple Ingredients you must exclude: Sugar Level of strictness for ingredients: Strict Favourite Meals: Pancakes Meal type: Preferred Cuisine type: }";
-// await createAndRun(process.env.ASSISTANT_ID, body);
