3a5a82ec94e374031d398d4f2f2a3e545a9b1c13
Nathan <nbel806@aucklanduni.ac.nz>
2024-04-22 15:46:01 +1200

    feat: remix meal endpoint

--- backend/src/routes/api/generation/remix.js
+++ backend/src/routes/api/generation/remix.js
@@ -1,5 +1,10 @@
 import express from "express";
 
+import createAndRun from "../../../openAI/openAi.js";
 const remix = express.Router();
+
+remix.get("/", async (req, res) => {
+	res.send(await createAndRun(process.env.ASSISTANT_REMIX_ID, JSON.stringify(req.body)));
+});
 
 export default remix;
