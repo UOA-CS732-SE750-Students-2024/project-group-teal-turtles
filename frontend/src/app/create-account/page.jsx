5ab8904d057132c01da62bcfd4c56614682deaf4
Dan <dcha762@aucklanduni.ac.nz>
2024-04-23 15:13:46 +1200

    feat: ui improvements

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -5,33 +5,28 @@ import React from "react";
 function CreateAccount() {
 	return (
 		<Stack height="100vh" justifyContent="center" alignItems="center" display="flex">
-			<Card sx={{ p: 6, borderRadius: 6 }}>
-				<Stack alignItems="center" spacing={1}>
-					<Typography variant="h4">Sign Up</Typography>
-					<Typography variant="body2">No Account? We'll create one for you automatically.</Typography>
-					<Typography variant="body1" fontWeight="bold" alignSelf="flex-start">
-						Username
-					</Typography>
-					<TextField fullWidth size="small" label="Username" />
-					<Typography variant="body1" fontWeight="bold" alignSelf="flex-start">
-						Email
-					</Typography>
-					<TextField fullWidth size="small" label="Email Address" />
-					<Typography variant="body1" fontWeight="bold" alignSelf="flex-start">
-						Password
-					</Typography>
-					<TextField fullWidth size="small" label="Password" />
-					<Button fullWidth variant="contained" sx={{ textTransform: "none" }}>
-						Create account
-					</Button>
-					<Typography>OR</Typography>
+			<Card sx={{ p: 4, borderRadius: 2 }}>
+				<Stack spacing={4}>
+					<Stack alignItems="center" spacing={1.5}>
+						<Typography variant="h4">Sign Up</Typography>
+						<Typography variant="body2">No Account? We'll create one for you automatically.</Typography>
+						<TextField fullWidth size="small" label="Username" />
+						<TextField fullWidth size="small" label="Email Address" />
+						<TextField fullWidth size="small" label="Password" />
+					</Stack>
+					<Stack width="100%" alignItems="center" spacing={1}>
+						<Button fullWidth variant="contained" sx={{ textTransform: "none" }}>
+							Create account
+						</Button>
+						<Typography alignSelf="center">OR</Typography>
 
-					<Button fullWidth variant="contained" endIcon={<Google />} sx={{ textTransform: "none" }}>
-						Sign in with Google
-					</Button>
-					<Button href="/login" sx={{ textTransform: "none" }}>
-						Sign in to existing account
-					</Button>
+						<Button fullWidth variant="contained" endIcon={<Google />} sx={{ textTransform: "none" }}>
+							Sign in with Google
+						</Button>
+						<Button fullWidth href="/login" sx={{ textTransform: "none" }}>
+							Sign in to existing account
+						</Button>
+					</Stack>
 				</Stack>
 			</Card>
 		</Stack>
--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -5,28 +5,24 @@ import React from "react";
 function Login() {
 	return (
 		<Stack height="100vh" justifyContent="center" alignItems="center" display="flex">
-			<Card sx={{ p: 6, borderRadius: 6 }}>
-				<Stack alignItems="center" spacing={1}>
+			<Card sx={{ p: 4, borderRadius: 2 }}>
+				<Stack alignItems="center" spacing={2}>
 					<Typography variant="h4">Login</Typography>
 					<Typography variant="body2">Sign in to access saved data and information.</Typography>
-					<Typography variant="body1" fontWeight="bold" alignSelf="flex-start">
-						Email
-					</Typography>
 					<TextField fullWidth size="small" label="Email Address" />
-					<Typography variant="body1" fontWeight="bold" alignSelf="flex-start">
-						Password
-					</Typography>
 					<TextField fullWidth size="small" label="Password" />
-					<Button fullWidth variant="contained" sx={{ textTransform: "none" }}>
-						Sign in
-					</Button>
-					<Typography>OR</Typography>
-					<Button fullWidth variant="contained" endIcon={<Google />} sx={{ textTransform: "none" }}>
-						Sign in with Google
-					</Button>
-					<Button href="/create-account" sx={{ textTransform: "none" }}>
-						Create a new account
-					</Button>
+					<Stack width="100%" alignItems="center" spacing={1}>
+						<Button fullWidth variant="contained" sx={{ textTransform: "none" }}>
+							Sign in
+						</Button>
+						<Typography>OR</Typography>
+						<Button fullWidth variant="contained" endIcon={<Google />} sx={{ textTransform: "none" }}>
+							Sign in with Google
+						</Button>
+						<Button fullWidth href="/create-account" sx={{ textTransform: "none" }}>
+							Create a new account
+						</Button>
+					</Stack>
 				</Stack>
 			</Card>
 		</Stack>
