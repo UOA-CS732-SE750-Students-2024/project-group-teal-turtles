8069d2e7b6bb1c76bde3b1d8601c870b2c96008b
Dan <dcha762@aucklanduni.ac.nz>
2024-04-23 19:19:57 +1200

    feat: password visibility toggle

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -1,10 +1,17 @@
 "use client";
 
 import { Google } from "@mui/icons-material";
-import { Stack, Typography, Card, Button, TextField, Divider } from "@mui/material";
-import React from "react";
+import { Stack, Typography, Card, Button, TextField, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import React, { useState } from "react";
 
 function CreateAccount() {
+	const [visible, setVisible] = useState(false);
+
+	const handleToggle = () => {
+		setVisible(!visible);
+	};
+
 	return (
 		<Stack height="100vh" justifyContent="center" alignItems="center" display="flex">
 			<Card sx={{ p: 6, borderRadius: 3 }}>
@@ -13,7 +20,16 @@ function CreateAccount() {
 					<Typography variant="h5">No Account? We'll create one for you automatically.</Typography>
 					<TextField fullWidth label="Username" />
 					<TextField fullWidth label="Email Address" />
-					<TextField fullWidth label="Password" />
+					<TextField
+						fullWidth
+						label="Password (8+ Characters)"
+						type={visible ? "text" : "password"}
+						InputProps={{
+							endAdornment: (
+								<IconButton onClick={handleToggle}>{visible ? <VisibilityOff /> : <Visibility />}</IconButton>
+							)
+						}}
+					/>
 					<Stack width="100%" alignItems="center" spacing={1.5}>
 						<Button fullWidth variant="contained" sx={{ textTransform: "none" }}>
 							<Typography variant="h6">Create Account</Typography>
--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,10 +1,17 @@
 "use client";
 
 import { Google } from "@mui/icons-material";
-import { Stack, Typography, Card, Button, TextField } from "@mui/material";
-import React from "react";
+import { Stack, Typography, Card, Button, TextField, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import React, { useState } from "react";
 
 function Login() {
+	const [visible, setVisible] = useState(false);
+
+	const handleToggle = () => {
+		setVisible(!visible);
+	};
+
 	return (
 		<Stack height="100vh" justifyContent="center" alignItems="center" display="flex">
 			<Card sx={{ p: 6, borderRadius: 3 }}>
@@ -12,7 +19,16 @@ function Login() {
 					<Typography variant="h3">Login</Typography>
 					<Typography variant="h5">Sign in to access saved data and information.</Typography>
 					<TextField fullWidth label="Email Address" />
-					<TextField fullWidth label="Password" />
+					<TextField
+						fullWidth
+						label="Password"
+						type={visible ? "text" : "password"}
+						InputProps={{
+							endAdornment: (
+								<IconButton onClick={handleToggle}>{visible ? <VisibilityOff /> : <Visibility />}</IconButton>
+							)
+						}}
+					/>
 					<Stack width="100%" alignItems="center" spacing={1.5}>
 						<Button fullWidth variant="contained" sx={{ textTransform: "none" }}>
 							<Typography variant="h6">Sign In</Typography>
