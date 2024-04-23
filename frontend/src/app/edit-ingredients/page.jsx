64f9fb9bd5bc74463aead47428753a67805e3baf
Jeremy <ctin678@aucklanduni.ac.nz>
2024-04-23 12:56:13 +1200

    feat: added Tabs to pantry page

--- frontend/src/app/edit-ingredients/page.jsx
+++ frontend/src/app/edit-ingredients/page.jsx
@@ -1,10 +1,21 @@
 import Layout from "@/components/Layout/Layout";
 import React from "react";
+import PantryTabs from "@/components/PantryTabs/PantryTabs";
+import { Box, Stack } from "@mui/material";
 
 function EditIngredients() {
 	return (
 		<Layout>
-			<div>EditIngredients</div>
+			<Stack
+				height="calc(100vh - 120px)"
+				sx={{ backgroundColor: "#FFFFFF" }}
+				alignItems="center"
+				justifyContent="center"
+			>
+				<Box sx={{ backgroundColor: "#FFF0FF", height: "100%", width: "100%", maxWidth: 1000 }}>
+					<PantryTabs />
+				</Box>
+			</Stack>
 		</Layout>
 	);
 }
