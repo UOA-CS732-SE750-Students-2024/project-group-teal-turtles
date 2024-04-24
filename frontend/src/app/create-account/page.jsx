d7b9265736db9f85060b883184ea1fd6380f4ef5
dcha762 <100822917+dcha762@users.noreply.github.com>
2024-04-24 15:28:08 +1200

    feat: login and register pages (#20)
    * feat: barebone register page
    
    * feat: add link to login
    
    * feat: login page
    
    * feat: ui improvements
    
    * feat: use client
    
    * fix: resize login page for full hd screen
    
    * feat: resized register page
    
    * feat: password visibility toggle
    
    * fix: sx styling
    
    * feat: usestate setup variables

--- frontend/src/app/create-account/page.jsx
+++ frontend/src/app/create-account/page.jsx
@@ -1,9 +1,71 @@
-"use client"
+"use client";
 
-import React from "react";
+import { Google } from "@mui/icons-material";
+import { Stack, Typography, Card, Button, TextField, IconButton, Link } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import React, { useState } from "react";
 
 function CreateAccount() {
-	return <div>Create Account</div>;
+	const [visible, setVisible] = useState(false);
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	return (
+		<Stack height="100vh" justifyContent="center" alignItems="center" display="flex">
+			<Card sx={{ p: 6, borderRadius: 3 }}>
+				<Stack alignItems="center" spacing={3}>
+					<Typography variant="h3">Sign Up</Typography>
+					<Typography variant="h5">Don't have an account? We'll create one for you.</Typography>
+					<TextField
+						fullWidth
+						label="Username"
+						value={username}
+						onChange={(event) => {
+							setUsername(event.target.value);
+						}}
+					/>
+					<TextField
+						fullWidth
+						label="Email Address"
+						value={email}
+						onChange={(event) => {
+							setEmail(event.target.value);
+						}}
+					/>
+					<TextField
+						fullWidth
+						label="Password (8+ Characters)"
+						type={visible ? "text" : "password"}
+						value={password}
+						onChange={(event) => {
+							setPassword(event.target.value);
+						}}
+						InputProps={{
+							endAdornment: (
+								<IconButton onClick={() => setVisible(!visible)}>
+									{visible ? <VisibilityOff /> : <Visibility />}
+								</IconButton>
+							)
+						}}
+					/>
+					<Stack width="100%" alignItems="center" spacing={1.5}>
+						<Button fullWidth variant="contained" sx={{ textTransform: "none", py: 1.5 }}>
+							<Typography variant="h6">Create Account</Typography>
+						</Button>
+						<Typography variant="h6">OR</Typography>
+
+						<Button fullWidth variant="contained" endIcon={<Google />} sx={{ textTransform: "none", py: 1.5 }}>
+							<Typography variant="h6">Sign in with Google</Typography>
+						</Button>
+						<Link href="/login" underline="hover">
+							<Typography variant="h6">Sign in to an existing account</Typography>
+						</Link>
+					</Stack>
+				</Stack>
+			</Card>
+		</Stack>
+	);
 }
 
 export default CreateAccount;
