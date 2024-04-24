4cd9dd74813260b5c45b4aa37ae886078810798d
Nathan <nbel806@aucklanduni.ac.nz>
2024-04-24 16:17:21 +1200

    test:prompt

--- backend/src/routes/api/generation/prompt.js
+++ backend/src/routes/api/generation/prompt.js
@@ -1,5 +1,10 @@
 import express from "express";
 
+import createAndRun from "../../../openAI/openAi.js";
 const prompt = express.Router();
+
+prompt.get("/", async (req, res) => {
+	res.send(await createAndRun(process.env.ASSISTANT_PROMPT_ID, JSON.stringify(req.body)));
+});
 
 export default prompt;
---
+++ backend/src/routes/api/generation/tests/prompt.test.js
@@ -0,0 +1,46 @@
+import express from "express";
+import request from "supertest";
+import routes from "../../../routes.js";
+
+const app = express();
+app.use(express.json());
+app.use("/", routes);
+
+describe("GET /remix", () => {
+	it("pancakes", (done) => {
+		const payload = {
+			prompt: "I would like to make some pancakes"
+		};
+
+		request(app)
+			.get("/api/generation/prompt")
+			.set("Content-Type", "application/json")
+			.send(JSON.stringify(payload))
+			.expect(200)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				console.log(res.text);
+				return done();
+			});
+	}, 15000);
+	it("beef nachos", (done) => {
+		const payload = {
+			prompt: "Beef Nachos"
+		};
+
+		request(app)
+			.get("/api/generation/prompt")
+			.set("Content-Type", "application/json")
+			.send(JSON.stringify(payload))
+			.expect(200)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				console.log(res.text);
+				return done();
+			});
+	}, 15000);
+});
