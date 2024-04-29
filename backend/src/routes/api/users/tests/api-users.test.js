e557819b3c5dc322de2db3d44f53815410c86e66
Nathan <nbel806@aucklanduni.ac.nz>
2024-04-29 21:18:59 +1200

    test: user test start

--- backend/src/routes/api/users/tests/api-users.test.js
+++ backend/src/routes/api/users/tests/api-users.test.js
@@ -6,3 +6,43 @@ import routes from "../../../routes.js";
 const app = express();
 app.use(express.json());
 app.use("/", routes);
+
+describe("GET /user", () => {
+	it("get exisiting user", (done) => {
+		const payload = {
+			uid: "id1"
+		};
+
+		request(app)
+			.get("/api/users/")
+			.set("Content-Type", "application/json")
+			.send(JSON.stringify(payload))
+			.expect(200)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				console.log(res.text);
+				return done();
+			});
+	}, 15000);
+
+	it("get non-exisiting user", (done) => {
+		const payload = {
+			uid: "userThatDoesNotExist"
+		};
+
+		request(app)
+			.get("/api/users/")
+			.set("Content-Type", "application/json")
+			.send(JSON.stringify(payload))
+			.expect(404)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				console.log(res.text);
+				return done();
+			});
+	}, 15000);
+});
