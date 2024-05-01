f7c0112bcbf2e2d14eef132c3a7ea40f8a66ad7c
Dan <dcha762@aucklanduni.ac.nz>
2024-05-01 16:07:36 +1200

    feat: wrap pantry item in cardactionarea

--- frontend/src/components/Pantry/PantryGrid.jsx
+++ frontend/src/components/Pantry/PantryGrid.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Card, Grid, Typography } from "@mui/material";
+import { Card, CardActionArea, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function PantryGrid({ itemData }) {
@@ -7,27 +7,29 @@ export default function PantryGrid({ itemData }) {
 		<Grid sx={{ flexGrow: 1 }} container spacing={2}>
 			{itemData.map((item) => (
 				<Grid item>
-					<Card
-						sx={{
-							height: 150,
-							width: 150,
-							backgroundColor: "#fff",
-							alignItems: "center",
-							justifyContent: "center",
-							display: "flex",
-							flexDirection: "column",
-							gap: "5",
-							transition: "background-color 0.3s",
-							"&:hover": {
-								cursor: "pointer",
-								backgroundColor: "#f0f0f0"
-							}
-						}}
-					>
-						<Image src={`${item.img}`} alt={item.title} width={100} height={100} priority />
-						<Typography color="#000" variant="body1" lineHeight={1}>
-							{item.title}
-						</Typography>
+					<Card>
+						<CardActionArea
+							sx={{
+								height: 150,
+								width: 150,
+								backgroundColor: "#fff",
+								alignItems: "center",
+								justifyContent: "center",
+								display: "flex",
+								flexDirection: "column",
+								gap: "5",
+								transition: "background-color 0.3s",
+								"&:hover": {
+									cursor: "pointer",
+									backgroundColor: "#f0f0f0"
+								}
+							}}
+						>
+							<Image src={`${item.img}`} alt={item.title} width={100} height={100} priority />
+							<Typography color="#000" variant="body1" lineHeight={1}>
+								{item.title}
+							</Typography>
+						</CardActionArea>
 					</Card>
 				</Grid>
 			))}
