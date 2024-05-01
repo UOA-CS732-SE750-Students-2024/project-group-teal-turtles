55c9546f34c7f4f65b408710c34f20e659808264
Dan <dcha762@aucklanduni.ac.nz>
2024-05-01 15:58:49 +1200

    feat: use cardwrapper for meal page

--- frontend/src/app/onboarding/page.jsx
+++ frontend/src/app/onboarding/page.jsx
@@ -1,20 +1,23 @@
+import CardWrapper from "@/components/CardWrapper/CardWrapper";
 import CategoryCard from "@/components/CategoryCard/CategoryCard";
 import { Grid, Stack, Typography, Button } from "@mui/material";
 
 function Onboarding() {
 	return (
-		<Stack alignItems="center">
-			<Typography variant="h2">Add Favourite Meals</Typography>
-			<Typography variant="h6">
-				Select Meals below that you love, in order to aid with recipe recommendation.
-			</Typography>
-			<Grid>
-				<CategoryCard text="Pizza" src="vercel.svg" onClick={() => {}} />
-			</Grid>
-			<Button fullWidth variant="contained" sx={{ textTransform: "none", py: 1.5 }}>
-				<Typography variant="h6">Continue</Typography>
-			</Button>
-		</Stack>
+		<CardWrapper>
+			<Stack alignItems="center">
+				<Typography variant="h2">Add Favourite Meals</Typography>
+				<Typography variant="h6">
+					Select Meals below that you love, in order to aid with recipe recommendation.
+				</Typography>
+				<Grid>
+					<CategoryCard text="Pizza" src="vercel.svg" onClick={() => {}} />
+				</Grid>
+				<Button fullWidth variant="contained" sx={{ textTransform: "none", py: 1.5 }}>
+					<Typography variant="h6">Continue</Typography>
+				</Button>
+			</Stack>
+		</CardWrapper>
 	);
 }
 
