cabe8b621b59cd05b77443c12d01050ad1f69969
Jeremy Ting <ctin678@aucklanduni.ac.nz>
2024-05-01 20:07:16 +1200

    feat: added icon

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -6,30 +6,8 @@ import StickyFooter from "./StickyFooter";
 export default function Layout({ children }) {
 	return (
 		<Box sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
-			{/* <header className={styles.header}>
-				This is a Header
-				<a href="/dashboard" className={styles.headeritem}>
-					Dashboard
-				</a>
-				<a href="/edit-profile" className={styles.headeritem}>
-					Edit Profile
-				</a>
-				<a href="/pantry" className={styles.headeritem}>
-					Pantry
-				</a>
-				<a href="/generation-options" className={styles.headeritem}>
-					Generation Options
-				</a>
-				<a href="/landing" className={styles.headeritem}>
-					Landing
-				</a>
-				<a href="/view-recipe" className={styles.headeritem}>
-					View Recipe
-				</a>
-			</header> */}
 			<ResponsiveAppBar />
 			<Box sx={{ flexGrow: "1" }}>{children}</Box>
-			{/* <BottomNavigation sx={{ flexGrow: "0" }}>This is a bottom navigation</BottomNavigation> */}
 			<StickyFooter />
 		</Box>
 	);
