fd8e3ad791f84bbae63e4bbefd1f0218a14d3b68
Nathan <nbel806@aucklanduni.ac.nz>
2024-05-02 14:50:52 +1200

    chore: deploy to PORT

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,4 +5,4 @@ dotenv.config();
 
 await mongoose.connect(process.env.DB_URL);
 const PORT = process.env.PORT ?? 3000;
-app.listen(3000, () => console.log(`Express Server is up and running on ${PORT}`));
+app.listen(PORT, () => console.log(`Express Server is up and running on ${PORT}`));
