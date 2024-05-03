75b046cc0ff3ab6fd7354a4a16c6ba6218238502
Jeremy Ting <ctin678@aucklanduni.ac.nz>
2024-05-03 15:59:01 +1200

    feat: changed checkbox to changing color, in process of updating images

--- frontend/src/components/Pantry/PantryGrid.jsx
+++ frontend/src/components/Pantry/PantryGrid.jsx
@@ -1,8 +1,10 @@
 import * as React from "react";
-import { Card, CardActionArea, Checkbox, Grid, Typography } from "@mui/material";
+import { Card, CardActionArea, Grid, Typography, alpha, useTheme } from "@mui/material";
 import Image from "next/image";
 
 export default function PantryGrid({ itemData, variant, onClick, selected }) {
+	const theme = useTheme();
+
 	return (
 		<Grid
 			sx={{ flexGrow: 1, justifyContent: variant === "onboarding" ? "space-evenly" : "auto" }}
@@ -11,13 +13,23 @@ export default function PantryGrid({ itemData, variant, onClick, selected }) {
 		>
 			{itemData.map((item) => (
 				<Grid item key={item.title}>
-					<Card sx={{ position: "relative" }}>
+					<Card
+						sx={{
+							position: "relative",
+							backgroundColor: selected
+								? selected.includes(item.title)
+									? theme.palette.primary.main
+									: "#fff"
+								: "#fff",
+							color: selected ? (selected.includes(item.title) ? "#fff" : "#000") : "#000"
+						}}
+					>
 						<CardActionArea
 							onClick={() => onClick(item.title)}
 							sx={{
 								height: 150,
 								width: 150,
-								backgroundColor: "#FFFFFF",
+								backgroundColor: alpha("#fff", 0),
 								alignItems: "center",
 								justifyContent: "center",
 								display: "flex",
@@ -25,12 +37,19 @@ export default function PantryGrid({ itemData, variant, onClick, selected }) {
 								gap: "5"
 							}}
 						>
-							<Image src={`${item.img}`} alt={item.title} width={100} height={100} priority />
-							<Typography color="#000" variant="body1" lineHeight={1}>
+							<Image
+								src={`${item.img}`}
+								alt={item.title}
+								width={100}
+								height={100}
+								priority
+								style={{ filter: selected && selected.includes(item.title) ? "invert(100%)" : "none" }}
+							/>
+							<Typography variant="body1" lineHeight={1}>
 								{item.title}
 							</Typography>
 						</CardActionArea>
-						<Checkbox
+						{/* <Checkbox
 							checked={selected ? selected.includes(item.title) : false}
 							disableRipple
 							sx={{
@@ -43,7 +62,7 @@ export default function PantryGrid({ itemData, variant, onClick, selected }) {
 								},
 								"& .MuiSvgIcon-root": { fontSize: 28 }
 							}}
-						/>
+						/> */}
 					</Card>
 				</Grid>
 			))}
