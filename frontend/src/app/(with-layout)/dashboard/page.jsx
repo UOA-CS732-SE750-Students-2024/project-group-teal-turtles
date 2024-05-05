11176b80fd323094f198c550cbf20fbc2a3800d3
Nathan <nbel806@aucklanduni.ac.nz>
2024-05-05 15:40:21 +1200

    feat: adds users pantry to dash

--- frontend/src/app/(with-layout)/dashboard/page.jsx
+++ frontend/src/app/(with-layout)/dashboard/page.jsx
@@ -1,15 +1,20 @@
 "use client";
 
-import IngredientSelector from "@/components/IngredientSelector/IngredientSelector";
 import MealGenerationPanel from "@/components/MealGenerationPanel/MealGenerationPanel";
+import UserPantryGrid from "@/components/PantryDash/UserPantryGrid";
+import { Stack } from "@mui/material";
 import React from "react";
 
 function Dashboard() {
 	return (
-		<>
+		<Stack
+			sx={{ backgroundColor: "#FFFFFF", minHeight: "calc(100vh - 70px)" }}
+			alignItems="center"
+			justifyContent="center"
+		>
 			<MealGenerationPanel />
-			<IngredientSelector />
-		</>
+			<UserPantryGrid />
+		</Stack>
 	);
 }
 
