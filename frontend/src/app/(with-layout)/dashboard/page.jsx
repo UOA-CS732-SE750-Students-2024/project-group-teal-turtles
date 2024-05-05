860c96161cb2d03d6c4e454637fdbd5ae33dd8de
tmur647 <100457787+tmur647@users.noreply.github.com>
2024-05-05 18:02:33 +1200

    fix: navigation (#41)
    * fix: change navigation from links to router
    
    * feat: remove navigation settings from profile
    
    * feat: running with auth token in local storage takes to landing page as well as fix sign in navigation
    
    * fix: dont need to show no parameters anymore
    
    ---------
    
    Co-authored-by: nbel806 <100734619+nbel806@users.noreply.github.com>

--- frontend/src/app/(with-layout)/dashboard/page.jsx
+++ frontend/src/app/(with-layout)/dashboard/page.jsx
@@ -4,14 +4,47 @@ import MealGenerationPanel from "@/components/MealGenerationPanel/MealGeneration
 import UserPantryGrid from "@/components/PantryDash/UserPantryGrid";
 import { Stack } from "@mui/material";
 import React from "react";
+import { Button } from "@mui/material";
+import { logout } from "@/app/auth-functions";
+import useDataStore from "@/lib/store";
+import { useRouter } from "next/navigation";
 
 function Dashboard() {
+	const {
+		setUserGeneratedMeals,
+		setUserDislikedIngredients,
+		setUserEmail,
+		setUserFavouriteMeals,
+		setUserIngredients,
+		setUserParameters,
+		setAuthToken
+	} = useDataStore();
+	const router = useRouter();
+	const handleLogout = async () => {
+		try {
+			await logout();
+			setUserGeneratedMeals([]);
+			setUserDislikedIngredients([]);
+			setUserEmail(null);
+			setUserFavouriteMeals([]);
+			setUserIngredients([]);
+			setUserParameters(null);
+			setAuthToken(null);
+			router.push("/landing");
+			console.log("logout successful");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
+			
 		<Stack
 			sx={{ backgroundColor: "#FFFFFF", minHeight: "calc(100vh - 70px)" }}
 			alignItems="center"
 			justifyContent="center"
 		>
+        <Button onClick={handleLogout}>Logout</Button>
 			<MealGenerationPanel />
 			<UserPantryGrid />
 		</Stack>
