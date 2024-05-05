72749ef263052eff70144dc99e487e4cb81e52ad
tmur647 <tmur647@aucklanduni.ac.nz>
2024-05-05 17:53:25 +1200

    feat: running with auth token in local storage takes to landing page as well as fix sign in navigation

--- frontend/src/app/(with-layout)/dashboard/page.jsx
+++ frontend/src/app/(with-layout)/dashboard/page.jsx
@@ -3,10 +3,42 @@
 import IngredientSelector from "@/components/IngredientSelector/IngredientSelector";
 import MealGenerationPanel from "@/components/MealGenerationPanel/MealGenerationPanel";
 import React from "react";
+import { Button } from "@mui/material";
+import { logout } from "@/app/auth-functions";
+import useDataStore from "@/lib/store";
+import { useRouter } from "next/navigation";
 
 function Dashboard() {
+	const {
+		setUserGeneratedMeals,
+		setUserDislikedIngredients,
+		setUserEmail,
+		setUserFavouriteMeals,
+		setUserIngredients,
+		setUserParameters,
+		setAuthToken
+	} = useDataStore();
+	const router = useRouter();
+	const handleLogout = async () => {
+		try {
+			await logout();
+			setUserGeneratedMeals([]);
+			setUserDislikedIngredients([]);
+			setUserEmail(null);
+			setUserFavouriteMeals([]);
+			setUserIngredients([]);
+			setUserParameters(null);
+			setAuthToken(null);
+			router.push("/landing");
+			console.log("logout successful");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<>
+			<Button onClick={handleLogout}>Logout</Button>
 			<MealGenerationPanel />
 			<IngredientSelector />
 		</>
