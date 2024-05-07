7a7f350c1b19c749ac036be75b2495e998b33340
Jeremy Ting <ctin678@aucklanduni.ac.nz>
2024-05-07 15:48:54 +1200

    feat: replaced user avatar with image

--- frontend/src/components/EditProfilePage/ProfileSummary/ProfileSummary.jsx
+++ frontend/src/components/EditProfilePage/ProfileSummary/ProfileSummary.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Typography, Avatar, Stack } from "@mui/material";
+import Image from "next/image";
 
 function ProfileSummary() {
 	return (
 		<Stack direction="column" spacing={2} alignItems="center">
-			<Avatar alt="User Avatar" src="/avatar.jpg" sx={{ height: "10vw", width: "10vw" }} />
+			<Image src={"/user.png"} alt={"User"} width={150} height={150} style={{ filter: "invert(100%)" }} />
 			<Typography component="h1" variant="h5">
 				John Doe
 			</Typography>
