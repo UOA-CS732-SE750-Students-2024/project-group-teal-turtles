f9a93374c37d03bb25b081ad2001ff6053589a3a
Jason <jasxavier7@gmail.com>
2024-05-07 13:49:04 +1200

    feat: added a logout button to the profile page

--- frontend/src/components/EditProfilePage/EditProfilePage.jsx
+++ frontend/src/components/EditProfilePage/EditProfilePage.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import IngredientSummary from "../IngredientSummary/IngredientSummary";
 import ProfileSummary from "../ProfileSummary/ProfileSummary";
 import { Stack } from "@mui/system";
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
 
 function EditProfilePage() {
+	const handleLogout = () => {
+		console.log("LOGOUT HERE");
+	};
+
 	return (
 		<>
-			<Stack direction="column" spacing={4} alignItems="center" sx={{ width: "40%", margin: "0 auto", padding: "10vh" }}>
+			<Stack
+				direction="column"
+				spacing={4}
+				alignItems="center"
+				sx={{ width: "40%", margin: "0 auto", padding: "10vh" }}
+			>
 				<ProfileSummary />
-				<Typography variant="h5">
-					Pantry
-				</Typography>
+				<Button variant="contained" onClick={handleLogout}>
+					Logout
+				</Button>
+				<Typography variant="h5">Pantry</Typography>
 				<IngredientSummary />
 			</Stack>
 		</>
