1671814512b290f751d37024e6d4f81a15cc2c9d
Jeremy Ting <ctin678@aucklanduni.ac.nz>
2024-05-07 12:41:26 +1200

    feat: moved pantrygrid to dashboard page and restyled

--- frontend/src/app/(with-layout)/dashboard/page.jsx
+++ frontend/src/app/(with-layout)/dashboard/page.jsx
@@ -1,10 +1,13 @@
 "use client";
-
 import MealGenerationPanel from "@/components/MealGenerationPanel/MealGenerationPanel";
-import UserPantryGrid from "@/components/PantryDash/UserPantryGrid";
-import { Divider, Stack, Typography } from "@mui/material";
+import { Box, Divider, Fab, Stack, Typography } from "@mui/material";
 import React from "react";
 import { Button, Container } from "@mui/material";
+import { useRouter } from "next/navigation";
+import AddIcon from "@mui/icons-material/Add";
+import PantryGrid from "@/components/Pantry/PantryGrid";
+import ingredients from "../../../ingredients.json";
+import useDataStore from "@/lib/store";
 
 const styles = {
 	container: {
@@ -12,11 +15,16 @@ const styles = {
 		flexDirection: "column",
 		justifyContent: "center",
 		alignItems: "center",
-		minHeight: "calc(100vh - 70px)"
+		minHeight: "calc(100vh - 70px)",
+		py: 5
 	}
 };
 
 function Dashboard() {
+	const router = useRouter();
+	const { userIngredients } = useDataStore();
+	let ingredientsPantry = ingredients.filter((item) => userIngredients.includes(item.title));
+
 	return (
 		<Stack alignItems="center" justifyContent="center">
 			<Container sx={styles.container}>
@@ -27,7 +35,24 @@ function Dashboard() {
 			</Container>
 			<Divider orientation="horizontal" variant="middle" width="600px" />
 			<Container sx={styles.container}>
-				<UserPantryGrid />
+				<Typography variant="h2" align="center" gutterBottom>
+					Your Pantry
+				</Typography>
+
+				{userIngredients && userIngredients.length > 0 ? (
+					<Box sx={{ backgroundColor: "grey.300", p: 2, borderRadius: 1 }}>
+						<PantryGrid
+							itemData={ingredientsPantry}
+							onClick={() => router.push("/pantry")}
+							selected={userIngredients}
+						/>
+					</Box>
+				) : (
+					<Button onClick={() => router.push("/pantry")} variant="contained" sx={{ p: 2 }}>
+						<AddIcon sx={{ mr: 1 }} />
+						Add Ingredients
+					</Button>
+				)}
 			</Container>
 		</Stack>
 	);
