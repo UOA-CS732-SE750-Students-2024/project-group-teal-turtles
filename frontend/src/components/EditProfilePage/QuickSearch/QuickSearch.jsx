ce584ece9c1eccd2642d6a0de4bbdd081deddf0c
Jason <jasxavier7@gmail.com>
2024-05-08 15:35:25 +1200

    feat: combined components together into functional quicksearch

--- frontend/src/components/EditProfilePage/QuickSearch/QuickSearch.jsx
+++ frontend/src/components/EditProfilePage/QuickSearch/QuickSearch.jsx
@@ -2,17 +2,33 @@ import React from "react";
 import { searchIngredients } from "./SearchUtil";
 import { TextField } from "@mui/material";
 import SearchResults from "./SearchResults";
-import ingredients from "@/ingredients.json";
+import SelectedIngredients from "./SelectedIngredients";
 
 function QuickSearch() {
 	const [searchTerm, setSearchTerm] = React.useState("");
-	const searchResults = searchTerm == "" ? ingredients : searchIngredients(searchTerm);
+	const [selectedIngredients, setSelectedIngredients] = React.useState([]);
+
+	const handleSelectIngredient = (ingredient) => {
+		return () => {
+			const isSelected = selectedIngredients.includes(ingredient);
+			if (isSelected) {
+				setSelectedIngredients(selectedIngredients.filter((selected) => selected !== ingredient));
+			} else {
+				setSelectedIngredients([...selectedIngredients, ingredient]);
+			}
+			console.log(selectedIngredients);
+		};
+	};
 
 	return (
 		<>
 			<TextField label="Search Term" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-			{JSON.stringify(searchResults)}
-			<SearchResults searchResults={searchResults} />
+			<SelectedIngredients selectedIngredients={selectedIngredients} handleSelectIngredient={handleSelectIngredient} />
+			<SearchResults
+				searchTerm={searchTerm}
+				selectedIngredients={selectedIngredients}
+				handleSelectIngredient={handleSelectIngredient}
+			/>
 		</>
 	);
 }
