dadd18fd7ed998fab554382eb7f74f28690da2a9
tmur647 <tmur647@aucklanduni.ac.nz>
2024-05-09 09:32:12 +1200

    feat: redired user to landing if unauthorised for a page

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,9 +1,23 @@
+"use client";
 import React from "react";
 import ResponsiveAppBar from "./ResponsiveAppBar";
-import { BottomNavigation, Box } from "@mui/material";
+import { Box } from "@mui/material";
 import StickyFooter from "./StickyFooter";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 export default function Layout({ children }) {
+	const router = useRouter();
+	useEffect(() => {
+		onAuthStateChanged(getAuth(), () => {
+			const authUser = getAuth().currentUser;
+			if (!authUser) {
+				console.log("not authed");
+				router.push("/landing");
+			}
+		});
+	}, []);
 	return (
 		<Box sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
 			<ResponsiveAppBar />
