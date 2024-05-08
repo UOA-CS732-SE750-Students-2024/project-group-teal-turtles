d7807ca80ba7cc635c0805a6b8e77aefba9ad9f7
Jason <jasxavier7@gmail.com>
2024-05-08 15:34:32 +1200

    feat: made a component for displaying a list of ingredients

---
+++ frontend/src/components/EditProfilePage/QuickSearch/DisplayIngredients.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Grid, Card, CardMedia, CardActionArea } from "@mui/material";
+
+function DisplayIngredients({ ingredients, selectedIngredients, handleSelectIngredient }) {
+	return (
+		<Grid container spacing={2}>
+			{ingredients.map((ingredient, index) => (
+				<Grid item key={index}>
+					<DisplayIngredient
+						ingredient={ingredient}
+						isSelected={selectedIngredients.includes(ingredient.title)}
+						handleSelect={handleSelectIngredient(ingredient.title)}
+					/>
+				</Grid>
+			))}
+		</Grid>
+	);
+}
+
+function DisplayIngredient({ ingredient, isSelected, handleSelect }) {
+	return (
+		<Card
+			sx={{
+				width: 50,
+				height: 50,
+				backgroundColor: isSelected ? "primary.main" : "#fff",
+				color: isSelected ? "#fff" : "#000"
+			}}
+		>
+			<CardActionArea onClick={handleSelect}>
+				<CardMedia component="img" image={ingredient.img} alt={ingredient.title} />
+			</CardActionArea>
+		</Card>
+	);
+}
+
+export default DisplayIngredients;
