393946b0e60dbae7a4f8134d774f152da3fcf9f1
tmur647 <100457787+tmur647@users.noreply.github.com>
2024-05-08 12:05:48 +1200

    feat: add recipe page to nav bar and make recipes persist locally (#59)
    * feat: recipes persist on refresh and previous recipe added to nav bar
    
    * fix: remove commented out old code
    
    ---------
    
    Co-authored-by: Dan <dcha762@aucklanduni.ac.nz>

--- frontend/src/components/ViewMeal/ViewMealCard.jsx
+++ frontend/src/components/ViewMeal/ViewMealCard.jsx
@@ -12,6 +12,7 @@ import {
 	generateMealRemix,
 	generateMealStrict
 } from "@/helpers/dbCalls";
+import { useRouter } from "next/navigation";
 
 export default function ViewMealCard() {
 	function MealCard() {
@@ -23,95 +24,109 @@ export default function ViewMealCard() {
 			setUserGeneratedMeals,
 			userParameters,
 			userIngredients,
-			prompt
+			prompt,
+			lastMeal,
+			setLastMeal,
+			lastRecipe,
+			setLastRecipe,
+			setLastIngredientQuantities,
+			lastIngredientQuantities,
+			lastIngredientsUser,
+			setLastIngredientsUser,
+			lastIngredientsNeeded,
+			setLastIngredientsNeeded
 		} = useDataStore();
-		const [recipe, setRecipe] = useState([]);
-		const [ingredientQuantities, setIngredientQuantities] = useState([]);
-		const [mealName, setMealName] = useState("");
-		const [ingredientsUser, setIngredientsUser] = useState([]);
-		const [ingredientsNeeded, setIngredientsNeeded] = useState([]);
+
 		const searchParams = useSearchParams();
-		const [mealLoaded, setMealLoaded] = useState(false);
-		const [recipeLoaded, setRecipeLoaded] = useState(false);
+		const router = useRouter();
+		const [mealCurrentlyGenerating, setMealCurrentlyGenerating] = useState(false);
 
 		useEffect(() => {
-			try {
-				async function fetchMeal() {
-					const authToken = await getAuth().currentUser.getIdToken();
-					if (searchParams.get("generateOption") === "Remix") {
-						generateMealRemix(authToken, mealToRemix)
-							.then((res) => {
-								afterResult(res.data.mealName, res.data.ingredients);
-								fetchRecipe(res.data.mealName, res.data.ingredients);
-							})
-							.catch((err) => {
-								console.log(err);
-							});
-					} else if (searchParams.get("generateOption") === "Prompt") {
-						generateMealPrompt(authToken, prompt)
-							.then((res) => {
-								afterResult(res.data.mealName, res.data.ingredients);
-								fetchRecipe(res.data.mealName, res.data.ingredients);
-							})
-							.catch((err) => {
-								console.log(err);
-							});
-					} else if (searchParams.get("generateOption") === "Basic") {
-						const body = {
-							favouriteMeals: userFavouriteMeals,
-							generatedMeals: userGeneratedMeals,
-							ingredients: userIngredients,
-							dislikedIngredients: userDislikedIngredients,
-							mealType: userParameters.mealType,
-							cuisine: userParameters.cuisine,
-							dietaryRequirements: userParameters.dietaryRequirements
-						};
-						generateMealLoose(authToken, body)
-							.then((res) => {
-								setIngredientsNeeded(res.data.ingredientsNeeded);
-								afterResult(res.data.mealName, res.data.ingredientsUser);
-								fetchRecipe(res.data.mealName, res.data.ingredientsUser + res.data.ingredientsNeeded);
-							})
-							.catch((err) => {
-								console.log(err);
-							});
-					} else if (searchParams.get("generateOption") === "Strict") {
-						const body = {
-							favouriteMeals: userFavouriteMeals,
-							generatedMeals: userGeneratedMeals,
-							ingredients: userIngredients,
-							dislikedIngredients: userDislikedIngredients,
-							mealType: userParameters.mealType,
-							cuisine: userParameters.cuisine,
-							dietaryRequirements: userParameters.dietaryRequirements
-						};
-						generateMealStrict(authToken, body)
-							.then((res) => {
-								afterResult(res.data.mealName, res.data.ingredientsUser);
-								fetchRecipe(res.data.mealName, res.data.ingredientsUser);
-							})
-							.catch((err) => {
-								console.log(err);
-							});
+			if (searchParams.get("from") === "generation") {
+				setMealCurrentlyGenerating(true);
+				setLastMeal("");
+				setLastRecipe("");
+				setLastIngredientQuantities([]);
+				setLastIngredientsUser([]);
+				setLastIngredientsNeeded([]);
+				console.log("generating");
+				try {
+					async function fetchMeal() {
+						const authToken = await getAuth().currentUser.getIdToken();
+						if (searchParams.get("generateOption") === "Remix") {
+							generateMealRemix(authToken, mealToRemix)
+								.then((res) => {
+									afterResult(res.data.mealName, res.data.ingredients);
+									fetchRecipe(res.data.mealName, res.data.ingredients);
+								})
+								.catch((err) => {
+									console.log(err);
+								});
+						} else if (searchParams.get("generateOption") === "Prompt") {
+							generateMealPrompt(authToken, prompt)
+								.then((res) => {
+									afterResult(res.data.mealName, res.data.ingredients);
+									fetchRecipe(res.data.mealName, res.data.ingredients);
+								})
+								.catch((err) => {
+									console.log(err);
+								});
+						} else if (searchParams.get("generateOption") === "Basic") {
+							const body = {
+								favouriteMeals: userFavouriteMeals,
+								generatedMeals: userGeneratedMeals,
+								ingredients: userIngredients,
+								dislikedIngredients: userDislikedIngredients,
+								mealType: userParameters.mealType,
+								cuisine: userParameters.cuisine,
+								dietaryRequirements: userParameters.dietaryRequirements
+							};
+							generateMealLoose(authToken, body)
+								.then((res) => {
+									setLastIngredientsNeeded(res.data.ingredientsNeeded);
+									afterResult(res.data.mealName, res.data.ingredientsUser);
+									fetchRecipe(res.data.mealName, res.data.ingredientsUser + res.data.ingredientsNeeded);
+								})
+								.catch((err) => {
+									console.log(err);
+								});
+						} else if (searchParams.get("generateOption") === "Strict") {
+							const body = {
+								favouriteMeals: userFavouriteMeals,
+								generatedMeals: userGeneratedMeals,
+								ingredients: userIngredients,
+								dislikedIngredients: userDislikedIngredients,
+								mealType: userParameters.mealType,
+								cuisine: userParameters.cuisine,
+								dietaryRequirements: userParameters.dietaryRequirements
+							};
+							generateMealStrict(authToken, body)
+								.then((res) => {
+									afterResult(res.data.mealName, res.data.ingredientsUser);
+									fetchRecipe(res.data.mealName, res.data.ingredientsUser);
+								})
+								.catch((err) => {
+									console.log(err);
+								});
+						}
 					}
+					fetchMeal();
+				} catch (err) {
+					console.log(err);
 				}
-				fetchMeal();
-			} catch (err) {
-				console.log(err);
 			}
 		}, []);
 
 		async function afterResult(mealName, userIngredients) {
 			const authToken = await getAuth().currentUser.getIdToken();
 
-			setMealName(mealName);
-			setIngredientsUser(userIngredients);
-
 			saveParameters(authToken, userParameters);
 			addGeneratedMeal(authToken, mealName);
 
 			setUserGeneratedMeals([...userGeneratedMeals, mealName]);
-			setMealLoaded(true);
+
+			setLastMeal(mealName);
+			setLastIngredientsUser(userIngredients);
 		}
 
 		async function fetchRecipe(mealName, ingredients) {
@@ -119,9 +134,9 @@ export default function ViewMealCard() {
 			const body = { mealName: mealName, ingredients: ingredients, numberOfPeople: userParameters.numberOfPeople };
 			generateRecipe(authToken, body)
 				.then((res) => {
-					setRecipe(res.data.steps);
-					setIngredientQuantities(res.data.ingredientQuantities);
-					setRecipeLoaded(true);
+					router.replace(`/view-meal?generateOption=${searchParams.get("generateOption")}`);
+					setLastRecipe(res.data.steps);
+					setLastIngredientQuantities(res.data.ingredientQuantities);
 				})
 				.catch((err) => {
 					console.log(err);
@@ -130,7 +145,7 @@ export default function ViewMealCard() {
 
 		return (
 			<Paper elevation={4} align="center" sx={{ p: 4, m: 8, mt: 4, width: "50%" }}>
-				{!mealLoaded && (
+				{lastMeal === "" && mealCurrentlyGenerating && (
 					<>
 						<Typography variant="h4" align="center">
 							Generating a delicious meal...
@@ -138,27 +153,39 @@ export default function ViewMealCard() {
 						<LinearProgress sx={{ mt: 4 }} />
 					</>
 				)}
-				{mealLoaded && (
+
+				{lastMeal === "" && !mealCurrentlyGenerating && (
 					<>
-						<Typography variant="h2">{mealName}</Typography>
+						<Typography variant="h4" align="center">
+							Please go to Generate to create a personalised recipe
+						</Typography>
+						<Button variant="contained" sx={{ mt: 4 }} onClick={() => router.push("/generation-options")}>
+							Generate
+						</Button>
+					</>
+				)}
+
+				{lastMeal !== "" && (
+					<>
+						<Typography variant="h2">{lastMeal}</Typography>
 						<Stack alignItems={"flex-start"} sx={{ mt: 4 }}>
 							<Typography variant="h4">Ingredients</Typography>
-							<Typography variant="h6">User: {ingredientsUser.join(", ")}</Typography>
-							{ingredientsNeeded.length > 0 && (
+							<Typography variant="h6">User: {lastIngredientsUser.join(", ")}</Typography>
+							{lastIngredientsNeeded.length > 0 && (
 								<Typography variant="h6">
-									Needed: {ingredientsNeeded.join(", ")} {}
+									Needed: {lastIngredientsNeeded.join(", ")} {}
 								</Typography>
 							)}
 						</Stack>
 					</>
 				)}
-				{mealLoaded && !recipeLoaded && <CircularProgress sx={{ mt: 4 }} size={50} />}
+				{lastMeal !== "" && lastRecipe === "" && <CircularProgress sx={{ mt: 4 }} size={50} />}
 
-				{recipeLoaded && (
+				{lastRecipe !== "" && (
 					<Stack alignItems={"flex-start"} sx={{ mt: 4 }} direction={"row"}>
 						<Stack alignItems={"flex-start"} sx={{ mr: 4 }}>
 							<Typography variant="h4">Quantities</Typography>
-							{ingredientQuantities.map((ingredient, index) => (
+							{lastIngredientQuantities.map((ingredient, index) => (
 								<Typography key={index} variant="h6">
 									{ingredient}
 								</Typography>
@@ -166,7 +193,7 @@ export default function ViewMealCard() {
 						</Stack>
 						<Stack alignItems={"flex-start"}>
 							<Typography variant="h4">Instructions</Typography>
-							{recipe.map((step, index) => (
+							{lastRecipe.map((step, index) => (
 								<Typography key={index} variant="h6">
 									{step}
 								</Typography>
