40f3e19c5ab47c1f4adb8516665a5a22f0a7248c
Jason <jasxavier7@gmail.com>
2024-05-08 15:38:45 +1200

    feat: order of insertion and deletion is now maintained

--- frontend/src/components/EditProfilePage/QuickSearch/SelectedIngredients.jsx
+++ frontend/src/components/EditProfilePage/QuickSearch/SelectedIngredients.jsx
@@ -4,11 +4,17 @@ import { Divider } from "@mui/material";
 import allIngredients from "@/ingredients.json";
 
 function SelectedIngredients({ selectedIngredients, handleSelectIngredient }) {
-    if (!selectedIngredients.length) {
-        return null;
-    }
+	if (!selectedIngredients.length) {
+		return null;
+	}
 
-    const ingredients = allIngredients.filter((item) => selectedIngredients.includes(item.title));
+	const ingredients = allIngredients.filter((item) => selectedIngredients.includes(item.title));
+
+	ingredients.sort((a, b) => {
+		const indexA = selectedIngredients.indexOf(a.title);
+		const indexB = selectedIngredients.indexOf(b.title);
+		return indexA - indexB;
+	});
 
 	return (
 		<>
