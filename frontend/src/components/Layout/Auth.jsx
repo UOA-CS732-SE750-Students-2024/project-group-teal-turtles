6dbf173226a7541ebcf55ffc36574cc403dd85c9
Nathan <nbel806@aucklanduni.ac.nz>
2024-05-09 10:44:33 +1200

    fix:auth

--- frontend/src/components/Layout/Auth.jsx
+++ frontend/src/components/Layout/Auth.jsx
@@ -2,8 +2,11 @@
 import { useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
+import { auth } from "@/app/firebase-config";
 
 export default function Auth() {
+	auth.app;
+
 	const router = useRouter();
 	useEffect(() => {
 		onAuthStateChanged(getAuth(), () => {
