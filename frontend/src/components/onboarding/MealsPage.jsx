1e65cdb2853851dc9ea68e88ae46a35c8d5f7405
Jason <jasxavier7@gmail.com>
2024-05-09 15:33:18 +1200

    Jx/refactor/onboarding (#81)
    * refactor: extracted individual components from the onboarding page
    
    * refactor: moved pages into individual files
    
    * fix: fixed component name for onboarding page

---
+++ frontend/src/components/onboarding/MealsPage.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import PantryGrid from "@/components/Pantry/PantryGrid";
+import { Stack, Typography, Button } from "@mui/material";
+import useDataStore from "@/lib/store";
+import meals from "@/meals.json";
+
+export function MealsPage({ onPageChange }) {
+	const { setUserFavouriteMeals, userFavouriteMeals } = useDataStore();
+
+	const handleMealsChange = (item) => {
+		if (userFavouriteMeals.includes(item)) {
+			const newMeals = userFavouriteMeals.filter((meal) => meal !== item);
+			setUserFavouriteMeals(newMeals);
+		} else {
+			setUserFavouriteMeals([...userFavouriteMeals, item]);
+		}
+	};
+
+	return (
+		<>
+			<Stack alignItems="center">
+				<Typography variant="h2" fontWeight="bold" sx={{ color: "primary.main" }}>
+					Add Favourite Meals
+				</Typography>
+				<Typography variant="h6" textAlign="center">
+					Select some meals below that you love, in order to aid with recipe generation.
+				</Typography>
+			</Stack>
+			<Stack marginY="30px">
+				<PantryGrid itemData={meals} variant="onboarding" onClick={handleMealsChange} selected={userFavouriteMeals} />
+			</Stack>
+			<Button variant="contained" sx={{ textTransform: "none", py: 1.5, width: "50%" }} onClick={onPageChange}>
+				<Typography variant="h6">Continue</Typography>
+			</Button>
+		</>
+	);
+}
