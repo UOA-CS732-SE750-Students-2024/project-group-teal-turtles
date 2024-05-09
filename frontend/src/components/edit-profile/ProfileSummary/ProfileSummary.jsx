0ef530df1e39019b6cc6c6cef639da52efda5659
Dan <dcha762@aucklanduni.ac.nz>
2024-05-09 17:13:23 +1200

    feat: styled profile summary

--- frontend/src/components/edit-profile/ProfileSummary/ProfileSummary.jsx
+++ frontend/src/components/edit-profile/ProfileSummary/ProfileSummary.jsx
@@ -4,13 +4,12 @@ import Image from "next/image";
 
 function ProfileSummary(props) {
 	return (
-		<Stack direction="column" spacing={2} alignItems="center">
-			<Image src={"/user.png"} alt={"User"} width={150} height={150} style={{ filter: "invert(100%)" }} />
-			<Typography component="h1" variant="h5">
+		<Stack direction="column" alignItems="center">
+			<Image src={"/logo.png"} alt={"User"} width={200} height={200} style={{ filter: "invert(100%)" }} />
+			<Typography fontWeight="bold" variant="h3">
 				{props.username}
 			</Typography>
-			<Typography variant="body1">{props.email}</Typography>
-			{/* Add more profile information here */}
+			<Typography variant="h6">{props.email}</Typography>
 		</Stack>
 	);
 }
