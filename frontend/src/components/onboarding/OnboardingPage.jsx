31fc59fa48ebd5c4f96b92005774e33bbe9377e0
Jason <jasxavier7@gmail.com>
2024-05-09 15:23:30 +1200

    Revert "refactor: extracted individual components from the onboarding page"
    This reverts commit a625e7fe35f1b0177fe8ff79d54d52a2409c9e27.

--- frontend/src/components/onboarding/OnboardingPage.jsx
+++ frontend/src/components/onboarding/OnboardingPage.jsx
@@ -12,28 +12,8 @@ import meals from "@/meals.json";
 
 function Onboarding() {
 	const [page, setPage] = useState("meals");
-
-	const handlePageChange = (newPage) => {
-		return () => {
-			setPage(newPage);
-		};
-	};
-
-	return (
-		<CardWrapper>
-			<Stack alignItems="center" width="700px">
-				{page === "ingredients" ? (
-					<IngredientsPage onPageChange={handlePageChange("meals")} />
-				) : (
-					<MealsPage onPageChange={handlePageChange("ingredients")} />
-				)}
-			</Stack>
-		</CardWrapper>
-	);
-}
-
-function MealsPage({ onPageChange }) {
-	const { setUserFavouriteMeals, userFavouriteMeals } = useDataStore();
+	const router = useRouter();
+	const { userIngredients, setUserIngredients, setUserFavouriteMeals, userFavouriteMeals } = useDataStore();
 
 	const handleMealsChange = (item) => {
 		if (userFavouriteMeals.includes(item)) {
@@ -43,31 +23,6 @@ function MealsPage({ onPageChange }) {
 			setUserFavouriteMeals([...userFavouriteMeals, item]);
 		}
 	};
-
-	return (
-		<>
-			<Stack alignItems="center">
-				<Typography variant="h2" fontWeight="bold" sx={{ color: "primary.main" }}>
-					Add Favourite Meals
-				</Typography>
-				<Typography variant="h6" textAlign="center">
-					Select some meals below that you love, in order to aid with recipe generation.
-				</Typography>
-			</Stack>
-			<Stack marginY="30px">
-				<PantryGrid itemData={meals} variant="onboarding" onClick={handleMealsChange} selected={userFavouriteMeals} />
-			</Stack>
-			<Button variant="contained" sx={{ textTransform: "none", py: 1.5, width: "50%" }} onClick={onPageChange}>
-				<Typography variant="h6">Continue</Typography>
-			</Button>
-		</>
-	);
-}
-
-function IngredientsPage({ onPageChange }) {
-	const { userIngredients, setUserIngredients } = useDataStore();
-	const ingredientsOnboarding = ingredients.filter((item) => item.categories.includes("Onboarding"));
-	const router = useRouter();
 
 	const handleIngredientsChange = (item) => {
 		if (userIngredients.includes(item)) {
@@ -79,37 +34,78 @@ function IngredientsPage({ onPageChange }) {
 	};
 
 	return (
-		<>
-			<Stack alignItems="center">
-				<Stack direction="row" alignItems="center" width="100%" justifyContent="center">
-					<IconButton sx={{ height: "56px", marginRight: "44px", position: "relative" }} onClick={onPageChange}>
-						<ChevronLeft sx={{ fontSize: "40px", color: "secondary.dark" }} />
-					</IconButton>
-					<Typography variant="h2" marginRight="100px" fontWeight="bold" sx={{ color: "primary.main" }}>
-						Add Ingredients
-					</Typography>
-				</Stack>
-				<Typography variant="h6" textAlign="center">
-					Add ingredients that you have, in order to aid with recipe recommendation.
-				</Typography>
+		<CardWrapper>
+			<Stack alignItems="center" width="700px">
+				{page === "ingredients" ? (
+					<>
+						<Stack alignItems="center">
+							<Stack direction="row" alignItems="center" width="100%" justifyContent="center">
+								<IconButton
+									sx={{ height: "56px", marginRight: "44px", position: "relative" }}
+									onClick={() => {
+										setPage("meals");
+									}}
+								>
+									<ChevronLeft sx={{ fontSize: "40px", color: "secondary.dark" }} />
+								</IconButton>
+								<Typography variant="h2" marginRight="100px" fontWeight="bold" sx={{ color: "primary.main" }}>
+									Add Ingredients
+								</Typography>
+							</Stack>
+							<Typography variant="h6" textAlign="center">
+								Add ingredients that you have, in order to aid with recipe recommendation.
+							</Typography>
+						</Stack>
+						<Stack marginY="30px">
+							<PantryGrid
+								itemData={ingredientsOnboarding}
+								variant="onboarding"
+								onClick={handleIngredientsChange}
+								selected={userIngredients}
+							/>
+						</Stack>
+						<Button
+							variant="contained"
+							sx={{ textTransform: "none", py: 1.5, width: "50%" }}
+							onClick={() => router.push("/dashboard")}
+						>
+							<Typography variant="h6">Continue</Typography>
+						</Button>
+					</>
+				) : (
+					<>
+						<Stack alignItems="center">
+							<Typography variant="h2" fontWeight="bold" sx={{ color: "primary.main" }}>
+								Add Favourite Meals
+							</Typography>
+							<Typography variant="h6" textAlign="center">
+								Select some meals below that you love, in order to aid with recipe generation.
+							</Typography>
+						</Stack>
+						<Stack marginY="30px">
+							<PantryGrid
+								itemData={meals}
+								variant="onboarding"
+								onClick={handleMealsChange}
+								selected={userFavouriteMeals}
+							/>
+						</Stack>
+						<Button
+							variant="contained"
+							sx={{ textTransform: "none", py: 1.5, width: "50%" }}
+							onClick={() => {
+								setPage("ingredients");
+							}}
+						>
+							<Typography variant="h6">Continue</Typography>
+						</Button>
+					</>
+				)}
 			</Stack>
-			<Stack marginY="30px">
-				<PantryGrid
-					itemData={ingredientsOnboarding}
-					variant="onboarding"
-					onClick={handleIngredientsChange}
-					selected={userIngredients}
-				/>
-			</Stack>
-			<Button
-				variant="contained"
-				sx={{ textTransform: "none", py: 1.5, width: "50%" }}
-				onClick={() => router.push("/dashboard")}
-			>
-				<Typography variant="h6">Continue</Typography>
-			</Button>
-		</>
+		</CardWrapper>
 	);
 }
+
+const ingredientsOnboarding = ingredients.filter((item) => item.categories.includes("Onboarding"));
 
 export default Onboarding;
